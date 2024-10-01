8d0a67f4b98697a0c41b820943ee06ae16473421
Gbenga Opeyemi <opesam42@gmail.com>
2024-10-01 01:29:52 +0100

    Done with frontend, just form validation

--- contact.php
+++ contact.php
@@ -4,29 +4,35 @@ require('config.php');
 require('header.php');
 ?>
 
+<main class="md">
+    <form action="" method="post">
+        <h3>Send a Message</h3>
+        <label for="name">Name<br>
+            <input type="text" name="name">
+        </label>
+        <label for="email">Email<br>
+            <input type="email" name="email">
+        </label>
+        <label for="message">Message<br>
+            <textarea type="text" name="message" rows="7"></textarea>
+        </label>
+        <input type="submit" name="submit" value="Send message">
+    </form>
 
-<form action="" method="post">
-    <h3>Send a Message</h3>
-    <label for="name">Name<br>
-        <input type="text" name="name">
-    </label>
-    <label for="email">Email<br>
-        <input type="email" name="email">
-    </label>
-    <label for="message">Message<br>
-        <textarea type="text" name="message"></textarea>
-    </label>
-    <input type="submit" name="submit" value="Send message">
-</form>
+    <section class="contact-option">
+        <div>
+            <a class="" href="mailto:opesam42@gmail.com"><i class="fa fa-regular fa-envelope"></i> <strong>opesam42@gmail.com</strong></a>
+            <input id="textToCopy" type="text" value="opesam42@gmail.com" hidden readonly /><span class="copy" id="copyBtn"><i class="fa fa-regular fa-copy fa-lg"></i></span>
+            <div class="copied"></div>
+        </div>
+                                
+        <div>
+            <a href="#"><i class="fa fa-github"></i> Github</a>
+            <a href="#"><i class="fa fa-linkedin"></i> LinkedIn</a>
+        </div>
 
-<div>
-    <a class="lg" href="mailto:opesam42@gmail.com">opesam42@gmail.com</a>
-    <input id="textToCopy" type="text" value="opesam42@gmail.com" hidden readonly /><span class="copy" id="copyBtn"><i class="fa fa-regular fa-copy fa-lg"></i></span>
-</div>
-                        
-</div> 
-<a href="#">Github</a>
-<a href="#">LinkedIn</a>
+    </section>
+</main>
 
 <?php
 require('footer.php');
--- scripts/script.js
+++ scripts/script.js
@@ -14,17 +14,25 @@ toggleBars.forEach((icon)=>{
 
 // copy mail
 copyBtn = document.getElementById('copyBtn');
+
 function copyFunc(){
     textToCopy = document.getElementById('textToCopy').value;
     cpyResult = document.querySelector('.copied')
     
+    
     navigator.clipboard.writeText(textToCopy).then(() => {
         //show message when copying is successful
-
+        cpyResult.innerHTML = '<i style="font-size: .8em; font-weight:bold;">Copied!</i>';
+                
+        // Revert back to the original  after 2 seconds
+        setTimeout(() => {
+            cpyResult.innerHTML = ''
+        }, 2000);
     }).catch(err => {
         // if an error occurs
         console.log('Failed to copy: ', err);
     })
 }
 copyBtn.addEventListener('click', copyFunc);
-console.log( document.getElementById('textToCopy').value )
+// console.log( document.getElementById('textToCopy').value )
+console.log(copyBtn);
