f795934a3c91bb416a22ca6e2169642e82aa5ca1
Gbenga Opeyemi <opesam42@gmail.com>
2024-10-05 02:17:15 +0100

    update edit.php

--- admin/edit.php
+++ admin/edit.php
@@ -1,75 +1,89 @@
 <?php
 require('../config.php');
 
+session_start(); // Make sure to start the session
+
 $validproj = null; // initialize to prevent error
 
-//validate php
-if(isset($_GET['id'])){
-    if(!is_numeric($_GET['id']) || $_GET['id'] <= 0){
+// Validate ID
+if (isset($_GET['id'])) {
+    if (!is_numeric($_GET['id']) || $_GET['id'] <= 0) {
         header("Location: " . $config_basedir . "404.php");
         exit();
-    }else{
-        $validproj = intval( $_GET['id'] );
+    } else {
+        $validproj = intval($_GET['id']);
     }
-} else{
-    // if no id is provided, place a blank form for adding new project
+} else {
+    // If no ID is provided, redirect to add project
     header("Location: " . $config_basedir . "admin/add.php");
+    exit();
 }
 
+// Connecting to the database
+$db = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbdatabase);
 
-    $db = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbdatabase);
-    if(isset($_POST['submit'])){
-        $title = mysqli_real_escape_string($db, $_POST['title']);
-        $descr = mysqli_real_escape_string($db, $_POST['descr']);
-        $content = mysqli_real_escape_string($db, $_POST['content']);
-        $validproj = $_POST['validproj'];
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
+if (isset($_POST['submit'])) {
+    // Handle form submission
+    $title = mysqli_real_escape_string($db, $_POST['title']);
+    $descr = mysqli_real_escape_string($db, $_POST['descr']);
+    $content = mysqli_real_escape_string($db, $_POST['content']);
+    $validproj = $_POST['validproj'];
 
-        $sql = "UPDATE case_studies 
-        SET title = '$title', description = '$descr', content = '$content' 
-        WHERE case_studies.id=" . $validproj .";";
-    
-        mysqli_query($db, $sql);
+    $sql = "UPDATE case_studies 
+            SET title = '$title', description = '$descr', content = '$content' 
+            WHERE case_studies.id=" . $validproj . ";";
+
+    if (mysqli_query($db, $sql)) {
         header("Location: " . $config_basedir . "project.php?id=" . $validproj);
-    } else{
-        require('../include/header.php');
+        exit();
+    } else {
+        echo "Error updating record: " . mysqli_error($db);
     }
-    
+}
 
-// retrieving project information from database
+// Retrieve project information from the database
 $sql = "SELECT * FROM case_studies WHERE case_studies.id = " . $validproj . ";";
 $result = mysqli_query($db, $sql);
 $row = mysqli_fetch_assoc($result);
 $num_row = mysqli_num_rows($result);
 
+if ($num_row == 0) {
+    header("Location: " . $config_basedir . "404.php");
+    exit();
+}
 
-$contentSanitized = "<p>" . preg_replace("/\n/", "</p><p>", $row['content']);
+$contentSanitized = "<p>" . preg_replace("/\n/", "</p><p>", $row['content']) . "</p>";
 
-
-
-<?php if(!isset($_SESSION['USERID'])) {
+// Check if user is logged in
+if (!isset($_SESSION['USERID'])) {
     header("Location: " . $config_basedir . "admin/");
     exit();
-} else{ ?>
-<form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" method="post">
-    <input type="hidden" name="validproj" value="<?php echo $validproj; ?>">
-    <!-- add value $validproj to help pass it to the php form handler -->
-    <label for="title"> Title<br>
-        <input type="text" name="title" value="<?php echo $row['title']; ?>">
-    </label><br>
-    <label for="descr">Description<br>
-        <textarea type="text" name="descr"><?php echo $row['description']; ?></textarea>
-    </label><br>
-    <label for="content">Content<br>
-        <textarea name="content" id="editor" width="100%"><?php echo $contentSanitized; ?></textarea>
-    </label>
-    <input type="submit" name="submit">
-</form>
+} else {
+    require('../include/header.php'); // Move header include here
 
+    // Render the form
+    ?>
+    <form action="<?php echo htmlspecialchars($_SERVER['SCRIPT_NAME']); ?>" method="post">
+        <input type="hidden" name="validproj" value="<?php echo htmlspecialchars($validproj); ?>">
+        <label for="title"> Title<br>
+            <input type="text" name="title" value="<?php echo htmlspecialchars($row['title']); ?>">
+        </label><br>
+        <label for="descr">Description<br>
+            <textarea name="descr"><?php echo htmlspecialchars($row['description']); ?></textarea>
+        </label><br>
+        <label for="content">Content<br>
+            <textarea name="content" id="editor" width="100%"><?php echo $contentSanitized; ?></textarea>
+        </label>
+        <input type="submit" name="submit">
+    </form>
 
-<script src="../scripts/quill.js"></script>
-<?php } ?>
+    <script src="../scripts/quill.js"></script>
 
-<?php
-require('../include/footer.php');
+    <?php
+    require('../include/footer.php');
+}
 ?>
