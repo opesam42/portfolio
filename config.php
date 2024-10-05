ba0ab94f65fda9faaf76b4d0c9739f370b200e7f
Gbenga Opeyemi <opesam42@gmail.com>
2024-10-05 01:41:21 +0100

    added configuration file

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+//Configuration variables
+
+/* $dbhost = "localhost";
+$dbuser = "root";
+$dbpassword = "";
+$dbdatabase = "portfolio"; */
+
+
+$dbhost = "sql3.freesqldatabase.com";
+$dbuser = "sql3735427";
+$dbpassword = getenv('DB_PSWD');
+$dbdatabase = "sql3735427"; 
+
+$config_author = "Gbenga Opeyemi";
+$config_basedir = "https://gbenga.koyeb.app/";
+
+// $config_basedir = "http://127.0.0.1/projects/portfolio/";
+   
+
+?>
